0709139b29e66c750fbf1f019d59d4c8bc3bd532
okanotakaaki <okanotakaaki@okano-no-MacBook-Pro.local>
2017-04-26 00:03:18 +0900

    Initial commit

--- W06/main.js
+++ W06/main.js
@@ -14,7 +14,7 @@ function main()
     
     //camera2種類Orthographic or Perspective
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );  //cameraの位置
+    camera.position.set( 0, 0, 10 );  //cameraの位置
     scene.add( camera );             //cameraを追加
 
     //描画のためのクラスrenderer
@@ -23,14 +23,14 @@ function main()
     document.body.appendChild( renderer.domElement ); //ウェブページのbodyにrendererのdomElementを追加
 
     //Objectの構成
-    var vertices = [ [-1,1,0],[-1,-1,0],[1,-1,0],[1,1,0],[-1,1,-1],[-1,-1,-1],[1,-1,-1],[1,1,-1]  ]; //v0,v1,v2,v3,v4,v5,v6,v7
+    var vertices = [ [-1,1,0],[-1,-1,0],[1,-1,0],[1,1,0],[-1,1,1],[-1,-1,1],[1,-1,1],[1,1,1]  ]; //v0,v1,v2,v3,v4,v5,v6,v7
     var faces =    [ [0,1,2],[0,2,3],[4,5,1],[4,1,0],[7,6,5],[7,5,4],[3,2,6],[3,6,7],[4,0,3],[4,3,7],[2,1,5],[2,5,6] ];  //f0:v0,v1,v2
     
     var v0 = new THREE.Vector3().fromArray(vertices[0]);
     var v1 = new THREE.Vector3().fromArray(vertices[1]);
     var v2 = new THREE.Vector3().fromArray(vertices[2]);
     var v3 = new THREE.Vector3().fromArray(vertices[3]);
-    var v4 = new THREE.Vector3().fromArray(vertices[4]);
+    /*var v4 = new THREE.Vector3().fromArray(vertices[4]);
     var v5 = new THREE.Vector3().fromArray(vertices[5]);
     var v6 = new THREE.Vector3().fromArray(vertices[6]);
     var v7 = new THREE.Vector3().fromArray(vertices[7]);
@@ -38,8 +38,8 @@ function main()
     var id = faces;
     var f0 = new THREE.Face3(id[0][0],id[0][1],id[0][2]);
     var f1 = new THREE.Face3(id[1][0],id[1][1],id[1][2]);
-    var f2 = new THREE.Face3(id[2][0],id[2][1],id[2][2]);
-    var f3 = new THREE.Face3(id[3][0],id[3][1],id[3][2]);
+    //var f2 = new THREE.Face3(id[2][0],id[2][1],id[2][2]);
+    /*var f3 = new THREE.Face3(id[3][0],id[3][1],id[3][2]);
     var f4 = new THREE.Face3(id[4][0],id[4][1],id[4][2]);
     var f5 = new THREE.Face3(id[5][0],id[5][1],id[5][2]);
     var f6 = new THREE.Face3(id[6][0],id[6][1],id[6][2]);
@@ -55,15 +55,15 @@ function main()
     geometry.vertices.push(v1);
     geometry.vertices.push(v2);
     geometry.vertices.push(v3);
-    geometry.vertices.push(v4);
-    geometry.vertices.push(v5);
-    geometry.vertices.push(v6);
+    //geometry.vertices.push(v4);
+    //geometry.vertices.push(v5);
+    /*geometry.vertices.push(v6);
     geometry.vertices.push(v7);
-    geometry.vertices.push(v8);
+    geometry.vertices.push(v8);*/
     geometry.faces.push(f0);
     geometry.faces.push(f1);
-    geometry.faces.push(f2);
-    geometry.faces.push(f3);
+    //geometry.faces.push(f2);
+    /*geometry.faces.push(f3);
     geometry.faces.push(f4);
     geometry.faces.push(f5);
     geometry.faces.push(f6);
@@ -71,15 +71,15 @@ function main()
     geometry.faces.push(f8);
     geometry.faces.push(f9);
     geometry.faces.push(f10);
-    geometry.faces.push(f11);
+    geometry.faces.push(f11);*/
     
     
     var material = new THREE.MeshBasicMaterial();
     material.vertexColors = THREE.FaceColors;
     geometry.faces[0].color = new THREE.Color(1,0,0);
     geometry.faces[1].color = new THREE.Color(1,0,0);
-    geometry.faces[2].color = new THREE.Color(1,0,0);
-    geometry.faces[3].color = new THREE.Color(1,0,0);
+    //geometry.faces[2].color = new THREE.Color(1,0,0);
+    /*geometry.faces[3].color = new THREE.Color(1,0,0);
     geometry.faces[4].color = new THREE.Color(1,0,0);
     geometry.faces[5].color = new THREE.Color(1,0,0);
     geometry.faces[6].color = new THREE.Color(1,0,0);
@@ -87,7 +87,7 @@ function main()
     geometry.faces[8].color = new THREE.Color(1,0,0);
     geometry.faces[9].color = new THREE.Color(1,0,0);
     geometry.faces[10].color = new THREE.Color(1,0,0);
-    geometry.faces[11].color = new THREE.Color(1,0,0); 
+    geometry.faces[11].color = new THREE.Color(1,0,0); */
     
 
     //
