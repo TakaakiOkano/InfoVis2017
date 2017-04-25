9f32d144e518ecd8ac6cbec2e957ddd80f7b4fd4
okanotakaaki <okanotakaaki@okano-no-MacBook-Pro.local>
2017-04-26 00:14:48 +0900

    Initial commit

--- W06/main.js
+++ W06/main.js
@@ -34,20 +34,20 @@ function main()
     var v5 = new THREE.Vector3().fromArray(vertices[5]);
     var v6 = new THREE.Vector3().fromArray(vertices[6]);
     var v7 = new THREE.Vector3().fromArray(vertices[7]);
-    /*var v8 = new THREE.Vector3().fromArray(vertices[8]);*/
+    var v8 = new THREE.Vector3().fromArray(vertices[8]);
     var id = faces;
     var f0 = new THREE.Face3(id[0][0],id[0][1],id[0][2]);
     var f1 = new THREE.Face3(id[1][0],id[1][1],id[1][2]);
     var f2 = new THREE.Face3(id[2][0],id[2][1],id[2][2]);
     var f3 = new THREE.Face3(id[3][0],id[3][1],id[3][2]);
     var f4 = new THREE.Face3(id[4][0],id[4][1],id[4][2]);
-    /*var f5 = new THREE.Face3(id[5][0],id[5][1],id[5][2]);
+    var f5 = new THREE.Face3(id[5][0],id[5][1],id[5][2]);
     var f6 = new THREE.Face3(id[6][0],id[6][1],id[6][2]);
     var f7 = new THREE.Face3(id[7][0],id[7][1],id[7][2]);
     var f8 = new THREE.Face3(id[8][0],id[8][1],id[8][2]);
     var f9 = new THREE.Face3(id[9][0],id[9][1],id[9][2]);
     var f10= new THREE.Face3(id[10][0],id[10][1],id[10][2]);
-    var f11= new THREE.Face3(id[11][0],id[11][1],id[11][2]);*/
+    var f11= new THREE.Face3(id[11][0],id[11][1],id[11][2]);
 
     
     var geometry = new THREE.Geometry();
@@ -59,19 +59,19 @@ function main()
     geometry.vertices.push(v5);
     geometry.vertices.push(v6);
     geometry.vertices.push(v7);
-    /*geometry.vertices.push(v8);*/
+    geometry.vertices.push(v8);
     geometry.faces.push(f0);
     geometry.faces.push(f1);
     geometry.faces.push(f2);
     geometry.faces.push(f3);
     geometry.faces.push(f4);
-    /*geometry.faces.push(f5);
+    geometry.faces.push(f5);
     geometry.faces.push(f6);
     geometry.faces.push(f7);
     geometry.faces.push(f8);
     geometry.faces.push(f9);
     geometry.faces.push(f10);
-    geometry.faces.push(f11);*/
+    geometry.faces.push(f11);
     
     
     var material = new THREE.MeshBasicMaterial();
@@ -81,13 +81,13 @@ function main()
     geometry.faces[2].color = new THREE.Color(1,0,0);
     geometry.faces[3].color = new THREE.Color(1,0,0);
     geometry.faces[4].color = new THREE.Color(1,0,0);
-    /*geometry.faces[5].color = new THREE.Color(1,0,0);
+    geometry.faces[5].color = new THREE.Color(1,0,0);
     geometry.faces[6].color = new THREE.Color(1,0,0);
     geometry.faces[7].color = new THREE.Color(1,0,0);
     geometry.faces[8].color = new THREE.Color(1,0,0);
     geometry.faces[9].color = new THREE.Color(1,0,0);
     geometry.faces[10].color = new THREE.Color(1,0,0);
-    geometry.faces[11].color = new THREE.Color(1,0,0); */
+    geometry.faces[11].color = new THREE.Color(1,0,0); 
     
     material.side = THREE.DoubleSide;
     //
