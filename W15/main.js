59ad43f53f892b9e124a5f0335f0215813ba476a
岡野　孝昭 <170x108x@istc-a-014.epc.center.kobe-u.ac.jp>
2017-05-31 14:15:44 +0900

    Initial commit

--- W15/main.js
+++ W15/main.js
@@ -75,8 +75,8 @@ function main()
 	    Para = new newPara();
 	    var gui = new dat.GUI();
 	    //gui.addColor(Para, 'color').onChange(setValue);
-	    //gui.add(Para, 'isovalue', 0, 255).step(1).onChange(setValue);
-	    gui.add(Para, 'isovalue', 0, 255).onChange(setValue);
+	    gui.add(Para, 'isovalue', 0, 255).step(1).onChange(setValue);
+	    //gui.add(Para, 'isovalue', 0, 255).onChange(setValue);
 	    //gui.add(Para, 'Lambertian');
 	    gui.add(Para, 'Phong');
 	    gui.add(Para, 'Box').onChange(setValue);
